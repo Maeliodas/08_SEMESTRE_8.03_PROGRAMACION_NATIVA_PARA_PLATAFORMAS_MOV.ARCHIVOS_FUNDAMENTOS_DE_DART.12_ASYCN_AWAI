92fe0d13819866b713c1f68aaa6222c08b11ffc1
VICTOR MANUEL ITURBIDE MARIANO <iturbide.mariano.victor.manuel@gmail.com>
2026-03-02 07:04:03 -0600

    DECIMO SEGUNDO ARCHIVO ASYNC AWAIT EN DART

--- (12)_ASYCN_AWAIT.dart
+++ (12)_ASYCN_AWAIT.dart
@@ -1,1 +1,15 @@
-Nuevo archivo
+void main() async{ //para quitar la instancia de funcion debemos indicarle el async y el await 
+  print('Inicio del programa'); 
+  try { 
+    final value = await httpGet('http://api.victor.dev'); //Una forma es agregando el .then otra forma es usando funciones async y await 
+    print(value);
+    } catch(err) { 
+      print('Error: $err');
+      } 
+      print('Fin del programa'); 
+}
+Future<String> httpGet(String url) async{ //significa lo mismo que en js al añadir async 
+  await Future.delayed(const Duration(seconds: 2)); 
+  throw 'Error en la petición'; 
+  //return 'Tenemos un valor'; 
+}
